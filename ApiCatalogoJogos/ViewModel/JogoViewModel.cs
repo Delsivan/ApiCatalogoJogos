ee78dbec904faade3ff2bb4df20bfa54c596cff9
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-11-02 11:12:30 -0300

    Adicionando jogosController, IJogoRepository e IJogoService

--- ApiCatalogoJogos/ViewModel/JogoViewModel.cs
+++ ApiCatalogoJogos/ViewModel/JogoViewModel.cs
@@ -7,11 +7,9 @@ namespace ApiCatalogoJogos.ViewModel
 {
     public class JogoViewModel
     {
-        public GuidId { get; set; }
+        public Guid Id { get; set; }
         public string Nome { get; set; }
         public string Produtora { get; set; }
         public double Preco { get; set; }
-
-
-}
+    }
 }
